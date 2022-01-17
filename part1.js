3f0f4863d66d8b9bc08ed5e9f900fd6ed8519594
Jim <jimps@inbox.ru>
2022-01-18 02:38:36 +0300

    Jimsaharov JS homework1

--- part1.js
+++ part1.js
@@ -1,9 +1,15 @@
-/**
- * return square of a number ex: square(2) => 4
- * return 0 if no value is given
- */
+
+  // return square of a number ex: square(2) => 4
+  // return 0 if no value is given
+ 
 function square(num) {
+  if (num === undefined){
+    return (0);
+  } else {
+    return Math.pow(num, 2); 
+  }
 }
+
 
 module.exports = {
   square
