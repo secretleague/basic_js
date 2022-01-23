a05b0b61647d5bc04d793a659ea8e11c0ac1f08e
ilya <avduevskiy@gmail.com>
2022-01-23 23:30:39 +0300

    avd111

--- part1.js
+++ part1.js
@@ -2,8 +2,15 @@
  * return square of a number ex: square(2) => 4
  * return 0 if no value is given
  */
-function square(num) {
+
+
+
+ function square(num){
+   if (typeof num !== 'number') return 0;
+  return num * num;
 }
+console.log(square(2));
+
 
 module.exports = {
   square
