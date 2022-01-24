098dfb61c95a525030c043165091c028b937030a
Jim <jimps@inbox.ru>
2022-01-24 13:14:01 +0300

    jimsaharov_part1

--- part1.js
+++ part1.js
@@ -1,16 +1,18 @@
-
   // return square of a number ex: square(2) => 4
   // return 0 if no value is given
  
 function square(num) {
   if (num === undefined){
-    return (0);
+    return 0;
   } else {
     return Math.pow(num, 2); 
   }
 }
 
 
+
 module.exports = {
   square
 }
+
+
--- part2.js
+++ part2.js
@@ -4,27 +4,36 @@
  * return 0 if no value is given
  */
 
-function calcSquare(obj) {
-}
+// function calcSquare(obj) {
+// }
 
 /**
  * accepts string as name, and should return a string "Hello, " and a name value
  * ex: greetings("Ivan") => "Hello, Ivan"
  * return empty string if no value is given
  */
-function greetings(name) {
-}
+
+
+// function greetings(name) {
+//   if (name == undefined){
+//     return String("");
+//   } else {
+//     return String("Hello, " + name); 
+//   } 
+// }
+
+// console.log(greetings("Ivan"))
 
 /**
  * accepts array of numbers and should return the sum of all numbers
  * ex: sum([1,2,3]) => 6
  * return 0 if no value is given
  */
-function sum(arr) {
-}
+// function sum(arr) {
+// }
 
- module.exports = {
-   calcSquare,
-   greetings,
-   sum
- }
+// module.exports = {
+//    calcSquare,
+//    greetings,
+//    sum
+//  }
