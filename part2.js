45fdcb2712e800bea8a04c8a21c6cd7b200362d2
Jim <jimps@inbox.ru>
2022-01-25 16:34:20 +0300

    homework inprogress psrt2

--- part2.js
+++ part2.js
@@ -3,9 +3,20 @@
  * returns height * width of this object, ex: calSquare({ width: 2, height: 3 }) => 6
  * return 0 if no value is given
  */
-
-// function calcSquare(obj) {
-// }
+ 
+ let obj = { 
+  height: 10, 
+  width: 3,
+}
+function calcSquare(obj) { 
+  
+  if (obj.height == undefined || obj.width == undefined){
+    return 0;
+  }  else {
+  return obj.height * obj.width;
+  }
+}
+console.log(calcSquare(obj));
 
 /**
  * accepts string as name, and should return a string "Hello, " and a name value
@@ -14,15 +25,15 @@
  */
 
 
-// function greetings(name) {
-//   if (name == undefined){
-//     return String("");
-//   } else {
-//     return String("Hello, " + name); 
-//   } 
-// }
+function greetings(name) {
+  if (name == undefined){
+    return ("");
+  } else {
+    return ("Hello, " + name); 
+  } 
+}
 
-// console.log(greetings("Ivan"))
+console.log(greetings("Ivan"))
 
 /**
  * accepts array of numbers and should return the sum of all numbers
@@ -32,8 +43,8 @@
 // function sum(arr) {
 // }
 
-// module.exports = {
-//    calcSquare,
-//    greetings,
-//    sum
-//  }
+module.exports = {
+   calcSquare,
+   greetings,
+   sum
+ }
